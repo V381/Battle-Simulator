f4f488b4ed333e38f09bc8c4ece33cbf5734c741
Pavle <V381@users.noreply.github.com>
2016-10-28 01:44:25 +0200

    Vehicle class update, Squad class update

--- simulator.js
+++ simulator.js
@@ -10,7 +10,19 @@ function Unit (opts) {
     }
 }
 
+function Unit (opts) {
+    for (var i in opts) {
+        if (opts.hasOwnProperty(i) && opts[i] !== undefined) this[i] = opts[i];
+    }
+}
+
 Unit.prototype = { 
+    get: function(key) {
+      var v = this[key];
+      if (typeof v === "function") return v();
+      else return v;
+    },
+    
     health: null, 
     rechargeCost: null, 
     active: true, 
@@ -24,10 +36,9 @@ Unit.prototype = {
         return Math.floor(Math.random() * diff) + from;  
     },
     
-    attack: function (unit) {
-        if (!unit.active) return;  
-        
-        if (unit.health <= 0) unit.active = false;
+    attack: function (unit) { 
+        if (!unit.get('active')) return; 
+        if (unit.get('health') <= 0) unit.active = false; 
     }
     
 }
@@ -42,6 +53,10 @@ Soldier.prototype = new Unit({ // Inherits
         var h = 1 + (health/100);
         var exp = this.random(50 + this.experience, 100);
         return 0.5 * h * exp / 100;
+    },
+    
+    damage : function(){
+        return 0.05 + this.experience / 100;
     }
 
 });
@@ -49,16 +64,13 @@ Soldier.prototype = new Unit({ // Inherits
 function Vehicle(opts) {
     Unit.call(this, opts);
     this.operators = [];
+    var maxOperators = Math.floor(Math.random() * 3) + 1;
+    for (var i = 0; i < maxOperators; i += 1) {
+        this.operators.push(new Soldier());
+    }
 }
 
 Vehicle.prototype = new Unit({
-    
-    addOperators : function(){
-        var maxOperators = Math.floor(Math.random() * 3) + 1;
-        for (var i = 0; i < maxOperators; i += 1) {
-            this.operators.push(new Soldier());
-        }
-    },
     
     hasOperators : function(){
         return this.operators.some(function (o) {
@@ -88,8 +100,77 @@ Vehicle.prototype = new Unit({
     },
     
     calculateDamage : function(power){
-        var result = (60 / 100) * this.health
-        var vehicleHealth = this.health; 
+        
+        var totalVehicleDamage = (this.health) - ((60 / 100) * power); 
+        this.health -= 0.6 * power; 
+        
+    
+        var ops = this.operators; // by taking the reference as a local variable, we don't have to do a lookup (faster)
+        var firstOpIndex = Math.floor(Math.random * ops.length);  // calculate the index of the op which will get hit first (see your own code)
+        var remainingDmg = 0.2 / (opts.length - 1); // calculate the remaning damage: 0.1 when two ops left, otherwise 0.2
+        ops.forEach(function (o, i) { // go through the ops, o is op, i is index 
+            if (i === firstOpIndex) o.health -= power * 0.2; // if the index is the same as the first hit op, apply 0.2
+            else { 
+                o.health -= remainingDmg * power; // otherwise apply whatever is left as damage either 0.1 or 0.2
+            }
+        });
+    },
+    
+    vehicleHealth: 100,
+    health: function () {
+      var avgOps = this.operators.reduce(function (p, o) { return p + o.health; }, 0) / this.operators.length;
+      return avgOps + this.vehicleHealth;
+    },
+    
+});
+
+
+function Squads(opts){
+    this.squad = [] 
+    
+}
+
+Squads.prototype = new Unit({
+    squad: null, 
+    isActive : function(){
+
+    },
+    generateSquad : function(){
+        var numberOfUnits = Math.floor(Math.random() * (10 - 5 + 1)) + 5; // The number of units per squad: 5 <= n <= 10
+        for(var i = 0; i <= numberOfUnits; i++){
+            if (i % 2 === 0){
+                this.squad.push(new Vehicle());
+            }else{
+                this.squad.push(new Soldier());
+            }
+        }
+
+        return this;
+    },
+    
+    gavg : function(){
+
+    },
+    
+    attackProbability : function(){
+
+    },
+    
+    damage : function(){
+        
+    },
+
+    attackStrategy : { 
+        random : function(){
+
+        },
+        weakest : function(){
+
+        },
+        strongest : function(){
+            
+        }
+            
     }
     
 });
